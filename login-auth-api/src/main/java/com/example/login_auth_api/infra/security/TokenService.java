a1184343781cd4c7c754c7b27849c2a170a94c76
larialvesg <larissaalvesgeraldoo@gmail.com>
2025-02-06 16:48:17 -0300

    06/02/2025

---
+++ login-auth-api/src/main/java/com/example/login_auth_api/infra/security/TokenService.java
@@ -0,0 +1,11 @@
+package com.example.login_auth_api.infra.security;
+import com.example.login_auth_api.domain.user.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenService {
+    public String generateToken(User user){
+      
+
+    }
+}
